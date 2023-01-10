0d93d44c45377e3d7590c410beee8a80f77bcdf1
Mwimwii <mpnyirongo@gmail.com>
2023-01-10 18:37:20 +0200

    2 files modified

--- components/LiveBrowserWidth/index.tsx
+++ components/LiveBrowserWidth/index.tsx
@@ -1,60 +1,73 @@
+
+
 import React=require('react');
 import './style.css'
-
+ 
 export const useInnerWidth = () => {
-  const [innerWidth, setInnerWidth] = React.useState(window.innerWidth);
-  React.useEffect(() => {
-    function handleResize() {
-      setInnerWidth(window.innerWidth);
-    }
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [innerWidth]);
-
-  return [innerWidth, setInnerWidth];
+ const [innerWidth, setInnerWidth] = React.useState(window.innerWidth);
+ React.useEffect(() => {
+   function handleResize() {
+     setInnerWidth(window.innerWidth);
+   }
+   window.addEventListener('resize', handleResize);
+   return () => {
+     window.removeEventListener('resize', handleResize);
+   };
+ }, [innerWidth]);
+ 
+ return [innerWidth, setInnerWidth];
 };
-
-
-
-const LiveBrowserWidth = () => {
-  const [innerWidth, setInnerWidth] = useInnerWidth()
-  const containerRef = React.useRef(null)
-  const [containerHeight, setContainerHeight] = React.useState<string>(null)
-  const [nanError, setNanError] = React.useState(false)
-
-  React.useEffect(() => {
-    if (containerHeight) {
-      setContainerHeight(containerRef.current.clientHeight)
-    }
-  }, [])
-
-  React.useEffect(() => {
-    if(isNaN(Number(containerHeight))){
-      setNanError(true)
-    } else {
-      setNanError(false)
-      containerRef.current.style.height = containerHeight + 'px'
-    }
-  },[containerHeight])
-
-  const handleInputChange = (e) => { 
-    if(e.key === 'Backspace'){
-      setContainerHeight(1)
-    } else {
-      setContainerHeight(containerHeight + e.key)
-    }
-  }
-
-
-  return (
-    <div ref={containerRef} className="container">
-
-      <input type="number" min="0" onKeyDown={handleInputChange} defaultValue={containerHeight && cotainerHeight}/>
-      <p>Incorrect container height value, please enter a proper value int (0...)</p> 
-      <h1>InnerWidth: {innerWidth as number}</h1>
-    </div>) 
+ 
+const useContainerHeight = () => {
+ const containerRef = React.useRef(null)
+ const [containerHeight, setContainerHeight] = React.useState<string>(null)
+ const [nanError, setNanError] = React.useState(false)
+ 
+ React.useEffect(() => {
+   if (containerRef) {
+     setContainerHeight(containerRef.current.clientHeight)
+   }
+ }, [])
+ 
+ React.useEffect(() => {
+   if(isNaN(Number(containerHeight))){
+     setNanError(true)
+   } else {
+     setNanError(false)
+     containerRef.current.style.height = containerHeight + 'px'
+   }
+ },[containerHeight])
+ 
+ const handleInputChange = (e) => {
+   let currentHeight = containerHeight.toString()
+   currentHeight = currentHeight.slice(0,currentHeight.length-1)
+   console.log()
+   if(e.key === 'Backspace'){
+   setContainerHeight(currentHeight)
+   } else {
+     setContainerHeight(containerHeight + e.key)
+   }
+ }
+ 
+return {
+containerRef,
+containerHeight,
+nanError,
+handleInputChange,
 }
-
+ 
+}
+ 
+const LiveBrowserWidth = () => {
+ const [innerWidth, setInnerWidth] = useInnerWidth()
+ const {containerRef, containerHeight, nanError, handleInputChange} = useContainerHeight()
+  return (
+   <div ref={containerRef} className="container">
+ 
+     <input type="number" min="0" onKeyDown={handleInputChange} defaultValue={containerHeight && containerHeight}/>
+     {nanError && <p>Incorrect container height value, please enter a proper value int (0...)</p>}
+     <h1>InnerWidth: {innerWidth as number}</h1>
+   </div>)
+}
+ 
 export default LiveBrowserWidth
