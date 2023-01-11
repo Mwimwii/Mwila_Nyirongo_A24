7fafc90f7c190f9fda3fec57c5901c25434d9249
Mwimwii <mpnyirongo@gmail.com>
2023-01-11 07:13:36 +0200

    Added global divHeight

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import './style.css';
 import { LiveBrowserWidth } from './components'
 
 export default function App() {
+  let divHeight: number;
+  // @ts-ignore
+  window.setDivHeight = height => divHeight = height
   return (
     <div>
       <LiveBrowserWidth />
